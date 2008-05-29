6e52258ce79562c27ca0fc0dee89cec37afe7673
deraze_a <deraze_a@ef59df4d-e10a-dd11-bb5e-0017a4413770>
2008-05-29 23:41:47 +0000

    creation du dossier init, mv get_set.c get_local.c
    git-svn-id: svn://localhost/home/damax/svn/42sh@60 ef59df4d-e10a-dd11-bb5e-0017a4413770

--- builtins/update_local.c
+++ builtins/update_local.c
@@ -5,7 +5,7 @@
 ** Login   <deraze_a@epitech.net>
 ** 
 ** Started on  Wed May 28 21:49:05 2008 aymeric derazey
-** Last update Thu May 29 18:21:55 2008 aymeric derazey
+** Last update Fri May 30 01:39:40 2008 aymeric derazey
 */
 
 #include "42.h"
@@ -27,6 +27,7 @@ int	update_with_equal(t_info *info, char *str, int fetched)
   while ((str[len] != '=') && (str[len] != '\0'))
     len++;
   str[len] = '\0';
+  xfree(info->set[fetched]);
   info->set[fetched] = my_strcat_trois(str, "\t", str+len+1);
   return (EXIT_SUCCESS);
 }
@@ -35,6 +36,7 @@ int	update_without_equal(t_info *info, char *str, int fetched)
 {
   if (str == NULL)
     return (EXIT_FAILURE);
+  xfree(info->set[fetched]);
   info->set[fetched] = my_strdup(str);
   return (EXIT_SUCCESS);
 }
