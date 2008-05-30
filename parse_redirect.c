dbd0826e95669374a440dab148e2615f0572b97b
lefebv_l <lefebv_l@ef59df4d-e10a-dd11-bb5e-0017a4413770>
2008-05-30 22:48:48 +0000

    plouf
    git-svn-id: svn://localhost/home/damax/svn/42sh@68 ef59df4d-e10a-dd11-bb5e-0017a4413770

--- parse_redirect.c
+++ parse_redirect.c
@@ -5,25 +5,25 @@
 ** Login   <lefebv_l@epitech.net>
 ** 
 ** Started on  Fri May 30 19:44:59 2008 laurent lefebvre
-** Last update Fri May 30 20:13:07 2008 laurent lefebvre
+** Last update Sat May 31 00:48:13 2008 laurent lefebvre
 */
 
 #include "42.h"
 
 int	parse_redirect(t_info *info, char *str)
 {
-  int	i;
+  /*  int	i;
   char	**tab;
 
   i = 0;
   tab = str_to_wordtab(str, "<");
-  /*if (tab == NULL)
-    return (EXIT_FAILURE);*/
+  if (tab == NULL)
+    return (EXIT_FAILURE);
   while (tab[i] != NULL)
     {
       printf("i:%d - %s\n", i, tab[i]);
       i++;
     }
   free_tab(tab);
-  return (EXIT_SUCCESS);
+  return (EXIT_SUCCESS);*/
 }
