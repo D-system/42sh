3640ab1f817222012dd4ca6aa4f2abcf21ea16e0
brenne_t <brenne_t@ef59df4d-e10a-dd11-bb5e-0017a4413770>
2009-05-08 18:46:52 +0000

    test
    git-svn-id: svn://localhost/home/damax/svn/42sh@75 ef59df4d-e10a-dd11-bb5e-0017a4413770

--- main.c
+++ main.c
@@ -1,13 +1,12 @@
 /*
 ** main.c for 42sh in /u/epitech_2012/deraze_a/cu/rendu/c/42sh/gp2/work
-** 
+**
 ** Made by aymeric derazey
 ** Login   <deraze_a@epitech.net>
-** 
+**
 ** Started on  Fri May 30 00:34:15 2008 aymeric derazey
-** Last update Fri May 30 00:34:24 2008 aymeric derazey
+** Last update Fri May  8 20:46:05 2009 thomas brennetot
 */
-
 
 #include <stdlib.h>
 #include "42.h"
